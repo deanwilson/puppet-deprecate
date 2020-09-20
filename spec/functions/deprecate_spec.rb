c110e777cbd922e6396916749ee5d15279570fc8
Dean Wilson <dean.wilson@gmail.com>
2020-09-20 12:01:11 +0100

    Fix "RSpec/ImplicitExpect: Prefer is_expected.to over should." violations

--- spec/functions/deprecate_spec.rb
+++ spec/functions/deprecate_spec.rb
@@ -8,7 +8,7 @@ describe 'deprecate' do
     it 'returns nil' do
       Timecop.freeze(Time.local('2015-01-24'))
 
-      should run.with_params('2015-01-25', 'Remove Foo at the end of the contract.').and_return(nil)
+      is_expected.to run.with_params('2015-01-25', 'Remove Foo at the end of the contract.').and_return(nil)
 
       Timecop.return
     end
@@ -20,7 +20,7 @@ describe 'deprecate' do
     it 'fails' do
       Timecop.freeze(Time.local('2015-01-26'))
 
-      should run.with_params('2015-01-25', 'Remove Foo at the end of the contract.').and_return(nil)
+      is_expected.to run.with_params('2015-01-25', 'Remove Foo at the end of the contract.').and_return(nil)
 
       Timecop.return
     end
@@ -28,7 +28,7 @@ describe 'deprecate' do
     it 'fails and aborts the compiliation' do
       Timecop.freeze(Time.local('2015-01-26'))
 
-      should run.with_params('2015-01-25', 'Remove Foo at the end of the contract.', true).and_return(nil)
+      is_expected.to run.with_params('2015-01-25', 'Remove Foo at the end of the contract.', true).and_return(nil)
 
       Timecop.return
     end
@@ -38,11 +38,11 @@ describe 'deprecate' do
 
   describe 'handling expected error conditions' do
     it 'throws ArgumentError if not called with 2 or 3 arguments' do
-      should run.with_params.and_raise_error(ArgumentError)
+      is_expected.to run.with_params.and_raise_error(ArgumentError)
     end
 
     it 'throws ArgumentError if provided with an invalid date' do
-      should run.with_params('2015012512', 'Test').and_raise_error(ArgumentError)
+      is_expected.to run.with_params('2015012512', 'Test').and_raise_error(ArgumentError)
     end
   end
 end
